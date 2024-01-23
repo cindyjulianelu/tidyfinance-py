6d35f50c53dc04d7f5a4a966eca42a2523bee086
Cindy Lu <20672288+cindyjulianelu@users.noreply.github.com>
2024-01-23 11:11:45 -0500

    portfolio sorts
    code to be tested

--- 04_univar_portfolio_sorts.py
+++ 04_univar_portfolio_sorts.py
@@ -83,4 +83,192 @@ prettify_result(model_fit)
 
 # Functional programming for Portfolio Sorts
 def assign_portfolio(data, sorting_variable, n_portfolios):
+  breakpoints = np.quantile(
+    data[sorting_variable].dropna(), 
+    np.linspace(0, 1, n_portfolios + 1),
+    method = "linear"
+  )
+  assigned_portfolios = pd.cut(
+    data[sorting_variable], 
+    bins = breakpoints, 
+    labels = range(1, breakpoints.size), 
+    include_lowest = True, 
+    right = False
+  )
+  return assigned_portfolios
+
+# Use top function to sort stocks into 10 portfolios each month
+beta_portfolios = (data_for_sorts.groupby(
+  "month"
+).apply(
+  lambda x: x.assign(
+    portfolio = assign_portfolio(x, "beta_lag", 10)
+  )
+).reset_index().groupby(
+  ["portfolio", "month"]
+).apply(
+  lambda x: x.assign(
+    ret = np.average(
+      x["ret_excess"], weights = x["mktcap_lag"]
+    )
+  )
+).reset_index().merge(
+  factors_ff3_monthly, how = "left", 
+  on = "month"
+))
+
+# More performance evaluation, CAPM-adjusted alphas
+beta_portfolios_summary = (beta_portfolios.groupby(
+  "portfolio"
+).apply(
+  lambda x: x.assign(
+    alpha = sm.OLS.from_formula(
+      formula = "ret ~ 1 + mkt_excess", 
+      data = x
+    ).fit().params[0], 
+    beta = sm.OLS.from_formula(
+      formula = "ret ~ 1 + mkt_excess", 
+      data = x
+    ).fit().params[1],
+    ret = x["ret"].mean()
+).tail(1)
+).reset_index(
+  drop = True
+).get(
+  ["portfolio", "alpha", "beta", "ret"]
+)
+)
+
+# Illustrate CAPM alphas of beta sorted portfolios
+plot_beta_portfolios_summary(
+  ggplot(
+    beta_portfolios_summary, 
+    aes(
+      x = "portfolio", 
+      y = "alpha", 
+      fill = "portfolio"
+    )
+  ) + 
+  geom_bar(
+    stat = "identity"
+  ) + 
+  labs(
+    x = "Portfolio", 
+    y = "CAPM alpha", fill = "Portfolio", 
+    title = "CAPM alphas of beta-sorted portfolios"
+  )
+).scale_y_continuous(
+  labels = percent_format()
+).theme(
+  legend_position = "none"
+)
+plot_beta_portfolios_summary.draw()
+
+# The security market line and beta portfolios
+sml_capm = (sm.OLS.from_formula(
+  formula = "ret ~ 1 + beta", 
+  data - beta_portfolios_summary
+).fit().params)
+plot_sml_capm = (
+  ggplot(
+    beta_portfolios_summary, 
+    aes(
+      x = "beta", y = "ret", 
+      color = "portfolio"
+    )
+  ) + 
+  geom_point() + 
+  geom_abline(
+    intercept = 0, 
+    slope = factors_ff3_monthly["mkt_excess"].mean(), 
+    linetype = "solid"
+  ) + 
+  geom_abline(
+    intercept = sml_capm["Intercept"], 
+    slope = sml_capm["beta"], 
+    linetype = "dashed"
+  ) + 
+  labs(
+    x = "Beta", y = "Excess Return", 
+    color = "Portfolio", 
+    title = "Average portfolio excess returns and beta estimates"
+  ) + 
+  scale_x_continuous(
+    limits = (0, 2)
+  ) + 
+  scale_y_continuous(
+    labels = percent_format(), 
+    limits = (0, factors_ff3_monthly["mkt_excess"].mean())
+  )
+) 
+plot_sml_capm.draw()
+
+# Prove more evidence against CAPM predictions
+beta_longshort = (beta_portfolios.assign(
+  portfolio = lambda x: (
+    x["portfolio"].apply(
+      lambda y: high if y == x["portfolio"].max()
+      else ("low" if y == x["portfolio"].min()
+      else y)
+  )
+  )
+).query(
+  "portfolio in ['low', 'high']"
+).pivot_table(
+  index = "month", 
+  columns = "portfolio", 
+  values = "ret"
+).assign(
+  long_short = lambda x: x["high"] - x["low"]
+).merge(
+  factors_ff3_monthly, 
+  how = "left", on = "month"
+)
+)
+
+# There's no statistically significant returns
+model_fit = (sm.OLS.from_formula(
+  formula = "long_short ~ 1",
+  data = beta_longshort
+).fit(cov_type = "HAC", 
+cov_kwds = {"maxlags" : 1}))
+prettify_result(model_fit)
+
+# Annual returns of extreme beta portfolios
+beta_longshort_year = (beta_longshort.assign(
+  year = lambda x: x["month"].dt.year
+).groupby(
+  "year"
+).aggregate(
   
+).reset_index().melt(
+  id-vars = "year", var_name = "name", 
+  value_name = "value"
+))
+plot_beta_longshort_year = (
+  ggplot(
+    beta_longshort_year, 
+    aes(x = "year", 
+    y = "value", fill = "name")
+  ) + 
+  geom_col(
+    position = "dodge"
+  ) + 
+  facet_wrap(
+    "~name", ncol = 1
+  ) + 
+  labs(
+    x = "", y = "", 
+    title = "Annual returns of beta portfolios"
+  ) + 
+  scale_color_discrete(
+    guide = False
+  ) + 
+  scale_y_continuous(
+    labels = percent_format()
+  ) + 
+  theme(
+    legend_position = "none"
+  )
+)
+plot_beta_longshort_year.draw()
--- 06_value_bivar_sorts.py
+++ 06_value_bivar_sorts.py
@@ -1,1 +1,207 @@
 # 06_value_bivar_sorts.py
+import pandas as pd
+import numpy as np
+import datetime as dt
+import sqlite3
+
+# Data preparation
+tidy_finance = sqlite3.connect(
+  database = "data/tidy_finance_python.sqlite"
+)
+crsp_monthly = (pd.read_sql_query(
+  sql = ("Select permno, gvkey, noht, ret_excess, mktcap, "
+  "mktcap_lag, exchange FROM crsp_monthly"), 
+  con = tidy_finance, 
+  parse_dates = {"month"}
+))
+book_equity = (pd.read_sql_query(
+  sql = "SELECT gvkey, datadate, be FROM compustat", 
+  con = tidy_finance, 
+parse_dates = {"datadate"}
+).drpna().assign(
+  month = lambda x: (
+    pd.to_datetime(
+      x["datadate"]
+    ).dt.to_period("M").dt.to_timestamp()
+  )
+)
+
+# Book to market ratio, avoiding look-ahead bias
+me = (crsp_monthly.assign(
+  sorting_date = lambda x: 
+    x["month"] + pd.DateOffset(months = 1)
+).rename(
+  columns = {"mktcap" : 'me}
+).get(
+  ["permno", "sorting_date", "me"]
+))
+)
+bm = (book_equity.merge(
+  crsp_monthly, how = "inner", 
+  on = ["gvkey", "month"]
+).assign(
+  bm = lambda x: x["be"]/x["mktcap"], 
+  sorting_dte = lambda x: x["month"] + pd.DateOffset(
+    months = 6
+  )
+).assign(
+  comp_date = lambda x: x["sorting_date"]
+).get(
+  ["permno", "gvkey", "sorting_date", "comp_date", "bm"]
+))
+data_for_sorts = (crsp_monthly.merge(
+  bm, how = "left", 
+  left_on = ["permno", "gvkey", "month"], 
+  right_on = ["permno", "gvkey", "sorting_date"]
+).merge(
+  me, how = "left", 
+  left_on = ["permno", "month"], 
+  right_on = ["permno", "sorting_date"]
+).get(
+  ["permno", "gvkey", "month", "ret_excess", 
+  "mktcap_lag", "me", "bm", "exchange, 
+  "comp_date]
+))
+data_for_sorts = (data_for_sorts.sort_values(
+  by = ["permno", "gvkey", "month"]
+).groupby(
+  ["permno", "gvkey"]
+).apply(
+  lambda x: x.assign(
+    bm = x["bm"].fillnam(method = "ffill"), 
+    comp_date = x["comp_date"].fillna(method = "ffill")
+  )
+).reset_index(
+  drop = True
+).assign(
+  threshold_date = lambda x: (
+    x["month"] - pd.DateOffset(months = 12)
+  )
+).query(
+  "comp_date > threshold_date"
+).drop(
+  columns = ["comp_date", "threshold_date"]
+).dropna())
+
+# Compute breakpoints to separate portfolios
+def assign_portfolio(data, exchanges, sorting_variable, 
+n_portfolios):
+  breakpoints = (data.
+  query(
+    f"exchange in {exchanges}"
+  ).get(
+    sorting_variable
+  ).quantile(
+    np.linspace(0, 1, num = n_portfolios + 1, 
+    interpolation = "linear")
+  ).drop_duplicates())
+  breakpoints.iloc[0] = -np.Inf
+  breakpoints.iloc[breakpoints_size - 1] = np.Inf
+  assigned_portfolios = pd.cut(
+    data[sorting_variable], 
+    bins = breakpoints, 
+    labels = range(1, breakpoints.size), 
+    include_lowest = True, 
+    right = False
+  )
+  return assigned_portfolios
+
+# Independent bivariate sorts 
+value_portfolios = (data_for_sorts.groupby(
+  "month"
+).apply(
+  lambda x: x.assign(
+    portfolio_bm = assign_portfolio(
+      data = x, sorting_variable = "bm", 
+      n_portfolios = 5, 
+      exchanges = ["NYSE"]
+      
+    ), 
+    portfolio_me = assign_portfolio(
+      data = x, sorting_variable = "me", 
+      n_portfolios = 5, 
+      exchanges = ["NYSE"]
+    )
+  )
+).reset_index(
+  drop = True
+).groupby(
+  ["month", "portfolio_bm", "portfolio_me"]
+).apply(
+  lambda x: pd.Series(
+    {
+      "ret" : np.average(x["ret_excess"], 
+      weights = x["mktcap_lag"])
+    }
+  )
+).reset_index())
+
+# Compute value premium after weighting porfolios
+value_premium = (value_portfolios.groupby(
+  ["month", "portfolio_bm"]
+).aggregate({"ret" : "mean"}
+).reset_index().gropuby(
+  "month"
+).apply(
+  lambda x: pd.Series({
+    "value_premium" : (
+      x.loc[x["portfolio_bm"] == 
+      x["portfolio_bm"].max(), "ret"].mean() - 
+      x.loc[x["portfolio_bm"] == 
+      x["portfolio_bm"].min(), "ret"].mean9)
+    )
+  })
+).aggregate(
+  {"value_premium" : "mean"}
+))
+
+# Dependent sorts, now consider second variable in assignment
+value_portfolios = (data_for_sorts.groupby(
+  "month"
+).apply(
+  lambda x: x.assign(
+    portfolio_me = assign_portfolio(
+      data = x, sorting_variable = "me", 
+      n_portfolios = 5, 
+      exchanges = ["NYSE"]
+    )
+  )
+).reset_index(
+  drop = True
+).groupby(
+  ["month", "portfolio_me"]
+).apply(
+  lambda x: x.assign(
+    portfolio_bm = assign_portfolio(
+      data = x, sorting_variable = "bm", 
+      n_portfolios = 5, 
+      exchanges = ["NYSE"]
+    )
+  )
+).reset_index(
+  drop = True
+).groupby(
+  ["month", "portfolio_bm", "portfolio_me"]
+).apply(
+  lambda x: pd.Series({
+    "ret" : np.average(
+      x["ret_excess"], weights = x["mktcap_lag"]
+    )
+  })
+).reset_index())
+value_premium = (value_portfolios.groupby(
+  ["month", "portfolio_bm"]
+).aggregate(
+  {"ret", "mean"}
+).reset_index().groupby(
+  "month"
+).apply(
+  lambda x: pd.Series({
+    x.loc[x["portfolio_bm"] == 
+    x["portfolio_bm"].max(), "ret"].mean() - 
+    x.loc[x["portfolio_bm"] == 
+    x["portfolio_bm"].min(), "ret"].mean()
+  })
+).aggregate(
+  {"value_premium" : "mean"}
+))
